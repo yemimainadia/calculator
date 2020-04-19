c811c3fea5bcbcbf613469ad37c4f613f45f20a1
yemimainadia <ratnasari.yemima@gmail.com>
2020-04-19 18:30:22 +0700

    mengubah hasil angka ke tulisan

--- main.js
+++ main.js
@@ -2,6 +2,36 @@
 var kamusDataSatuan = ["nol", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh"]
 
 
+function angkaKeTulisan(jumlahDigit, hasilOperasi) {
+    var hasilTulisan;
+    if (jumlahDigit == 1) {
+        if (hasilOperasi == 0) {
+            hasilTulisan = kamusDataSatuan[0];
+        } else if (hasilOperasi == 1) {
+            hasilTulisan = kamusDataSatuan[1];
+        } else if (hasilOperasi == 2) {
+            hasilTulisan = kamusDataSatuan[2];
+        } else if (hasilOperasi == 3) {
+            hasilTulisan = kamusDataSatuan[3];
+        } else if (hasilOperasi == 4) {
+            hasilTulisan = kamusDataSatuan[4];
+        } else if (hasilOperasi == 5) {
+            hasilTulisan = kamusDataSatuan[5];
+        } else if (hasilOperasi == 6) {
+            hasilTulisan = kamusDataSatuan[6];
+        } else if (hasilOperasi == 7) {
+            hasilTulisan = kamusDataSatuan[7];
+        } else if (hasilOperasi == 8) {
+            hasilTulisan = kamusDataSatuan[8];
+        } else if (hasilOperasi == 9) {
+            hasilTulisan = kamusDataSatuan[9];
+        } else {
+            alert('system error');
+        }
+    }
+    return hasilTulisan;
+}
+
 
 // membuat fungsi penjumlahan
 function calcPlus() {
@@ -9,39 +39,12 @@ function calcPlus() {
     console.log(iPlus1);
     var iPlus2 = parseInt(document.getElementById("inputPlus2").value);
     console.log(iPlus2);
-    var tempResult = iPlus1 + iPlus2;
+    var hasilOperasi = iPlus1 + iPlus2;
     // cara menghitung jumlah digit hasil penjumlahan
-    var tempResultDigit = tempResult.toString().length;
+    var jumlahDigit = hasilOperasi.toString().length;
 
-    // set variabel untuk menyimpan format hasil tulisan satuan
-    var hasilTulisan;
-    if (tempResultDigit == 1) {
-        if (tempResult == 0) {
-            hasilTulisan = kamusDataSatuan[0];
-        } else if (tempResult == 1) {
-            hasilTulisan = kamusDataSatuan[1];
-        } else if (tempResult == 2) {
-            hasilTulisan = kamusDataSatuan[2];
-        } else if (tempResult == 3) {
-            hasilTulisan = kamusDataSatuan[3];
-        } else if (tempResult == 4) {
-            hasilTulisan = kamusDataSatuan[4];
-        } else if (tempResult == 5) {
-            hasilTulisan = kamusDataSatuan[5];
-        } else if (tempResult == 6) {
-            hasilTulisan = kamusDataSatuan[6];
-        } else if (tempResult == 7) {
-            hasilTulisan = kamusDataSatuan[7];
-        } else if (tempResult == 8) {
-            hasilTulisan = kamusDataSatuan[8];
-        } else if (tempResult == 9) {
-            hasilTulisan = kamusDataSatuan[9];
-        } else {
-            alert('system error');
-        }
-    }
-
-    document.getElementById("outputPlusHasil").value = hasilTulisan;
+    // mengubah angka ke tulisan dan menampilkan
+    document.getElementById("outputPlusHasil").value = angkaKeTulisan(jumlahDigit, hasilOperasi);
 }
 
 
@@ -51,10 +54,12 @@ function calcMinus() {
     console.log(iMinus1);
     var iMinus2 = parseInt(document.getElementById("inputMinus2").value);
     console.log(iMinus2);
-    var tempResult = iMinus1 - iMinus2;
+    var hasilOperasi = iMinus1 - iMinus2;
     // cara menghitung jumlah digit hasil penjumlahan
-    var tempResultDigit = tempResult.toString().length;
-    document.getElementById("outputMinusHasil").value = tempResult;
+    var jumlahDigit = hasilOperasi.toString().length;
+    // mengubah angka ke tulisan dan menampilkan
+    document.getElementById("outputMinusHasil").value = angkaKeTulisan(jumlahDigit, hasilOperasi);
+
 }
 // membuat fungsi perkalian
 function calcMultiple() {
@@ -62,10 +67,12 @@ function calcMultiple() {
     console.log(iMultiple1);
     var iMultiple2 = parseInt(document.getElementById("inputMultiple2").value);
     console.log(iMultiple2);
-    var tempResult = iMultiple1 * iMultiple2;
+    var hasilOperasi = iMultiple1 * iMultiple2;
     // cara menghitung jumlah digit hasil penjumlahan
-    var tempResultDigit = tempResult.toString().length;
-    document.getElementById("outputMultipleHasil").value = tempResult;
+    var jumlahDigit = hasilOperasi.toString().length;
+    // mengubah angka ke tulisan dan menampilkan
+    document.getElementById("outputMultipleHasil").value = angkaKeTulisan(jumlahDigit, hasilOperasi);
+
 }
 
 // membuat fungsi pembagian
@@ -74,8 +81,10 @@ function calcDivide() {
     console.log(iDivide1);
     var iDivide2 = parseInt(document.getElementById("inputDivide2").value);
     console.log(iDivide2);
-    var tempResult = iDivide1 / iDivide2;
+    var hasilOperasi = iDivide1 / iDivide2;
     // cara menghitung jumlah digit hasil penjumlahan
-    var tempResultDigit = tempResult.toString().length;
-    document.getElementById("outputDivideHasil").value = tempResult;
+    var jumlahDigit = hasilOperasi.toString().length;
+    // mengubah angka ke tulisan dan menampilkan
+    document.getElementById("outputDivideHasil").value = angkaKeTulisan(jumlahDigit, hasilOperasi);
+
 }
